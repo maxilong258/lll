99b2042121170b6af67b97e73bafc4d5fdb775c9
maxilong <adc3080027554@gmail.com>
2026-07-22 10:16:36 +0800

    1

--- solutions/206. 反转链表.ts
+++ solutions/206. 反转链表.ts
@@ -11,3 +11,13 @@ function reverseList(head: ListNode | null): ListNode | null {
   }
   return prev;
 }
+
+// function reverseList(head: ListNode | null): ListNode | null {
+//   if (head === null || head.next === null) {
+//     return head;
+//   }
+//   const newHead = reverseList(head.next);
+//   head.next.next = head;
+//   head.next = null;
+//   return newHead;
+// }
