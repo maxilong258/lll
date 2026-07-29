196b82df31841b8b5d0131321af8906113340265
maxilong <adc3080027554@gmail.com>
2026-07-29 17:50:09 +0800

    1

--- solutions/41. 缺失的第一个正数.ts
+++ solutions/41. 缺失的第一个正数.ts
@@ -1,16 +1,22 @@
 function firstMissingPositive(nums: number[]): number {
-  const n = nums.length
+  const n = nums.length;
+
+  // 1. 将每个数字放到对应的索引位置上 (数字 x 放到 nums[x - 1])
   for (let i = 0; i < n; i++) {
     while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
-      const temp = nums[nums[i] - 1]
-      nums[nums[i] - 1] = nums[i]
-      nums[i] = temp
+      const temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
     }
   }
+
+  // 2. 检查哪个位置的数字不匹配
   for (let i = 0; i < n; i++) {
     if (nums[i] !== i + 1) {
-      return i + 1
+      return i + 1;
     }
   }
-  return n + 1
+
+  // 3. 如果 1 到 n 全部存在，则缺失的是 n + 1
+  return n + 1;
 }
