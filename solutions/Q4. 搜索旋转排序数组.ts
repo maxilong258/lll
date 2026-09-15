1bf2d4e1f33e90015d4c493e1e321f96fc128ffd
maxilong <adc3080027554@gmail.com>
2026-09-15 10:03:49 +0800

    1

---
+++ solutions/Q4. 搜索旋转排序数组.ts
@@ -0,0 +1,33 @@
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    // 判断左半部分是否有序
+    if (nums[left] <= nums[mid]) {
+      // target 是否落在左半有序区间内
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    // 右半部分有序
+    else {
+      // target 是否落在右半有序区间内
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+}
