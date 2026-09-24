da2141650669ee82f4e2619334512d9526f6af85
maxilong <adc3080027554@gmail.com>
2026-09-24 11:19:19 +0800

    1

---
+++ solutions/Q1. 合并两个有序数组.ts
@@ -0,0 +1,26 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let tail = m + n - 1;
+
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums1[p1] > nums2[p2]) {
+      nums1[tail] = nums1[p1];
+      p1--;
+    } else {
+      nums1[tail] = nums2[p2];
+      p2--;
+    }
+    tail--;
+  }
+
+  // 如果 nums2 还有剩余元素，直接填入 nums1
+  while (p2 >= 0) {
+    nums1[tail] = nums2[p2];
+    p2--;
+    tail--;
+  }
+}
